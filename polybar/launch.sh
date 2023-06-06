f9c6ccfc9687e7177f4b8689a0fc59d775106b73
Elizabeth Danzberger <spookystuff@protonmail.com>
2023-06-06 03:16:05 -0400

    upload dotfiles (again)

---
+++ polybar/launch.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+# Terminate already running bar instances
+# If all your bars have ipc enabled, you can use 
+polybar-msg cmd quit
+# Otherwise you can use the nuclear option:
+# killall -q polybar
+
+# launch main bar
+echo "---" | tee -a /tmp/polybar1.log
+polybar main 2>&1 | tee -a /tmp/polybar1.log & disown
+
+echo "Bars launched..."
